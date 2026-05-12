57981aa9cd27f1c8a3c8cc0e14497261768629d4
Joax2020 <joaquinclaros02@gmail.com>
2026-05-11 20:25:51 -0400

    Cambiando el puerto para brevo

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,8 +19,8 @@ const generarToken = (user) => {
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.BREVO_SMTP_USER,
         pass: process.env.BREVO_SMTP_PASS
